96a4b68ccb2fcdd3fe0e5f2a6b9243c7e60ff7e2
Charles Lauture <clauture@cnm.edu>
2020-09-22 15:41:10 -0400

    Done

---
+++ src/Demo/WelcomeMessage.java
@@ -0,0 +1,25 @@
+package Demo;
+
+public class WelcomeMessage {
+	
+	public static void main(String[] args) {
+		System.out.print("starting welcome message");
+		
+		//Declaring the variable
+		String name;
+		
+		//define the variable
+		name ="Charlie";
+		
+		//Declare & Define variable
+		String date = "Friday, 2:12pm";
+		double transactionAmount = 32.58;
+		
+		
+		//use the variable
+		System.out.print(name);
+		System.out.print(date);
+		System.out.print(transactionAmount);
+	}
+
+}
